bb6191c1cd37a65bf6efe9648e5d2c4a5b1ab2ff
MikhSuv <mischasuv@mail.ru>
2026-03-05 22:07:10 +0300

    Added nodes calculating functions

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,12 @@
 program main
-  use polynomial_interpolation, only: say_hello
+  use precision_mod
+  use polynomial_interpolation
   implicit none
+  integer :: n = 10
+  real(dp) :: a = -1.0_dp
+  real(dp) :: b = 1.0_dp
 
-  call say_hello()
+  print *, uniform_nodes(a, b, n)
+  print *, chebyshev_nodes(a, b, n)
+
 end program main
--- src/polynomial_interpolation.f90
+++ src/polynomial_interpolation.f90
@@ -1,10 +1,34 @@
 module polynomial_interpolation
+  use precision_mod
   implicit none
   private
 
-  public :: say_hello
+  public :: uniform_nodes, chebyshev_nodes
+  real(dp), parameter :: pi = 4.0_dp*atan(1.0_dp)
 contains
-  subroutine say_hello
-    print *, "Hello, polynomial_interpolation!"
-  end subroutine say_hello
+
+  function uniform_nodes(a, b, n) result(nodes)
+    real(dp), intent(in) :: a, b ! Начало и конец интервала интерполирования
+    integer, intent(in) :: n ! Количество интервалов
+    real(dp) :: nodes(0:n) ! Узлы равномерной сетки
+
+    integer :: k
+    real(dp) :: h ! Шаг сетки
+    h = (b - a)/real(n, dp)
+    do k = 0, n
+      nodes(k) = a + real(k, dp)*h
+    end do
+  end function uniform_nodes
+
+  function chebyshev_nodes(a, b, n) result(nodes)
+    real(dp), intent(in) :: a, b ! Начало и конец интервала интерполирования
+    integer, intent(in) :: n ! Количество интервалов
+    real(dp) :: nodes(0:n) ! Узлы чебышевской сетки
+
+    integer :: k
+
+    do k = 0, n
+      nodes(k) = 0.5_dp*(a+b) + 0.5_dp*(b-a)*cos(pi*real(2*k+1,dp)/real(2*n+2, dp))
+    end do
+  end function chebyshev_nodes
 end module polynomial_interpolation
